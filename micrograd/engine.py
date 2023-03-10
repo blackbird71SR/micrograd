4df10c7d2dc6437bcc4d9434fd151162f50c7e44
Omkar Ajnadkar <omkarajnadkar@gmail.com>
2023-03-09 21:01:07 -0600

    Initiate micrograd engine

---
+++ micrograd/engine.py
@@ -0,0 +1,20 @@
+class Value:
+
+  def __init__(self, data, _children=(), _op=''):
+    self.data = data
+    self.grad = 0
+    # internal variables for autograd graph construction
+    self._backward = lambda: None
+    self._prev = set(_children)
+    self._op = _op
+
+  def __add__(self, other):
+    other = other if isinstance(other, Value) else Value(other)
+    out = Value(self.data + other.data, (self, other), '+')
+
+    def _backward():
+      self.grad += out.grad
+      other.grad += out.grad 
+    out._backward = _backward
+
+    return out
